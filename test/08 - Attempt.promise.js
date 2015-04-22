ccbd4b5a39824c1f46d49b9824b97b820a6f4a7f
jaubourg <j@ubourg.net>
2015-04-22 03:50:12 +0200

    More promise integration testing

--- test/08 - Attempt.promise.js
+++ test/08 - Attempt.promise.js
@@ -38,3 +38,34 @@ createTests( {
 		}, 20 );
 	}
 } );
+
+createTests( {
+	target: module.exports,
+	possibilities: {
+		outcome: [ "success", "failure" ],
+		async: [ true, false ]
+	},
+	name: function( options ) {
+		return "new Attempt( promise )" + ( options.async ? " async " : " sync " ) + options.outcome;
+	},
+	test: function( options, __ ) {
+		__.expect( 1 );
+		function handler( type ) {
+			return options.outcome === type ? function( value ) {
+				__.strictEqual( value, "OK", type );
+			} : function() {
+				__.ok( false, type );
+			};
+		}
+		new Attempt( new Promise( function( success, failure ) {
+			var func = options.outcome === "success" ? success : failure;
+			if ( options.async ) {
+				setTimeout( func, 10, "OK" );
+			} else {
+				func( "OK" );
+			}
+		} ) ).success( handler( "success" ) ).failure( handler( "failure" ) ).always( function() {
+			__.done();
+		} );
+	}
+} );
