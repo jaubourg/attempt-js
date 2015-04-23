44afd694ba507085ccba85128bd59bc81b774360
jaubourg <j@ubourg.net>
2015-04-23 16:40:35 +0200

    Adds tape support

---
+++ test/tape/tapeToNodeunit.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var Test = require( "tape/lib/test" );
+
+var nodeunitToTapeMethod = {
+	"ok": true,
+	"equal": "deepLooseEqual",
+	"notEqual": "notDeepLooseEqual",
+	"strictEqual": "equal",
+	"notStrictEqual": "notEqual",
+	"deepEqual": true,
+	"notDeepEqual": true,
+	"throws": true,
+	"doesNotThrow": true,
+	"expect": "plan",
+	"done": "end"
+};
+
+function Nodeunit( tapeObject ) {
+	this.__tapeObject = tapeObject;
+}
+
+Nodeunit.prototype = {
+	expect: function( count ) {
+		this.__tapeObject.plan( count + 1 );
+	}
+};
+
+function createMethod( name ) {
+	var targetMethod = nodeunitToTapeMethod[ name ];
+	if ( targetMethod === true ) {
+		targetMethod = name;
+	}
+	targetMethod = Test.prototype[ targetMethod ];
+	Nodeunit.prototype[ name ] = function() {
+		return targetMethod.apply( this.__tapeObject, arguments );
+	};
+}
+
+for ( var name in nodeunitToTapeMethod ) {
+	if ( nodeunitToTapeMethod.hasOwnProperty( name ) ) {
+		createMethod( name, nodeunitToTapeMethod[ name ] );
+	}
+}
+
+module.exports = function( tapeObject ) {
+	return new Nodeunit( tapeObject );
+};
